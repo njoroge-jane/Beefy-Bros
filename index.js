ddbd46360534905ae4e7218b454a951e0fdc5b70
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2021-11-03 17:58:25 +0300

    Add cart and styles to cart

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-window.onscroll = function () {myFunction()};
-var navbar = document.getElementsByClassName("navbar");
-var sticky = navbar.offsetTop;
-function myFunction () {
-  if (window.pageOffset >= sticky) {
-    navbar.classList.add ("sticky");
-  }
-  else {
-    navbar.classList.remove("sticky");
+
+ window.onscroll = function(){
+var navbar = document.getElementsByClassName("navbar")[0];
+var fixed = navbar.offsetTop;
+console.log(fixed);
+ }
+ 
+// function myFunction () {
+//   if (window.pageOffset >= fixed) {
+//     navbar.classList.add ("fixed");
+//   }
+//   else {
+//     navbar.classList.remove ("fixed");
+//   }
+// }
+// document.getElementsByClassName ("navbar").onscroll = myFunction;
+// function hello () {
+//   if (document.getElementById("word").innerHTML === "BROS"){
+//     document.getElementById("word").innerHTML = "BB";
+//   }
+// else {document.getElementById("word").innerHTML = "BROS";
+// }
+// }
+// document.getElementById("word")
+document.addEventListener(onscroll, function (){
+  document.getElementsByTagName("nav").onscroll = myFunction;
+}
+
+)
+function myFunction(){
+  if (document.getElementsByTagName("nav").onscroll= true) {
+    document.getElementsByTagName("nav") = fixed
+
   }
 }
