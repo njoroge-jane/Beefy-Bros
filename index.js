7000d0df5dcef1b2d95b874bf486eb73b83505a1
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2021-11-10 14:28:18 +0300

    remove cart div

--- index.js
+++ index.js
@@ -2,42 +2,18 @@
  window.onscroll = function(){
 var navbar = document.getElementsByClassName("bodypart")[0];
 var fixed = navbar.offsetTop;
-var cart = document.getElementsByClassName("cart")[0]
-var 
+var cart = document.getElementsByClassName("cart")[0];
+var menu = document.getElementsByClassName("menu")[0];
 console.log(fixed);
 console.log(window.pageYOffset)
 if (window.pageYOffset>=fixed){
   cart.classList.add ("cart1");
+  menu.classList.add ("menu1");
 }
-else {cart.classList.remove("cart1")
+else {cart.classList.remove("cart1");
+menu.classList.remove("menu1");
 }
  }
  
-// // function myFunction () {
-// //   if (window.pageOffset >= fixed) {
-// //     navbar.classList.add ("fixed");
-// //   }
-// //   else {
-// //     navbar.classList.remove ("fixed");
-// //   }
-// // }
-// // document.getElementsByClassName ("navbar").onscroll = myFunction;
-// // function hello () {
-// //   if (document.getElementById("word").innerHTML === "BROS"){
-// //     document.getElementById("word").innerHTML = "BB";
-// //   }
-// // else {document.getElementById("word").innerHTML = "BROS";
-// // }
-// // }
-// // document.getElementById("word")
-// document.addEventListener(onscroll, function (){
-//   document.getElementsByTagName("nav").onscroll = myFunction;
-// }
+var buttons = document.querySelectorAll(".mainbody button");
 
-// )
-// function myFunction(){
-//   if (document.getElementsByTagName("nav").onscroll= true) {
-//     document.getElementsByTagName("nav") = fixed
-
-//   }
-// }
